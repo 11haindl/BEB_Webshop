8d37963e6d6eb08d45459a9116554ae80bf266b2
LS.Notebook <magicluks@live.at>
2023-05-11 09:02:44 +0200

    Projekt Struktur

---
+++ HAINDL_STUPPACHER_BE_Uebung3/product_kataloger/model/productType.php
@@ -0,0 +1,10 @@
+<?php
+
+class productType
+{
+    protected $id;
+
+    protected function __construct($id){
+
+    }
+}
---
+++ HAINDL_STUPPACHER_BE_Uebung3/product_kataloger/model/products.php
@@ -0,0 +1,11 @@
+<?php
+
+class products
+{
+    private $productTypeID;
+    private $name;
+
+    protected function __construct($name,$productTypeID){
+        this.$name = 
+    }
+}
