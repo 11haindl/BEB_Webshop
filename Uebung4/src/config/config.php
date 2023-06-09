81e8efa169e4fb1128f2a8cb28174de345536580
11haindl <11haindl@gmail.com>
2023-06-09 09:08:35 +0200

    added sample solution3 as base for exercise4

---
+++ Uebung4/src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DBHost", "localhost");
+define("DBName", "ss16-bbb2-fst-1;charset=utf8");
+define("DBPassword", "");
+define("DBUsername", "root");
+
+define("API_URL", "http://localhost/MusterloesungComposerUebung3/");
